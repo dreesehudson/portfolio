b814d6184fc84b64f30be4d74f8ddfb4b8c6d30f
Reese Hudson <david.reese.hudson@gmail.com>
2020-12-08 23:03:31 -0500

    add icons, links, content, firebase

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,52 +1,67 @@
 import React from 'react';
 import { Card, CardText, CardBody, CardTitle, Button, Row, Container, Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faGitAlt } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faGitAlt, faKaggle } from '@fortawesome/free-brands-svg-icons'
 import { faReact, faJsSquare, faBootstrap, faHtml5, faCss3Alt, faPhp, faLaravel } from '@fortawesome/free-brands-svg-icons'
-import { faDatabase, faFireAlt } from '@fortawesome/free-solid-svg-icons'
+import { faDatabase, faExclamationTriangle, faFireAlt } from '@fortawesome/free-solid-svg-icons'
 import projectData from '../projectData';
 
 function ProjectExt() {
-
-    function splitStack(stack) { 
-        // console.log(stack)
-        stack.map((item) => {
-            // console.log(item)
+    function tech(stack) {
+        return stack.map((item) => {
             let icon = ''
             if (item === "React") {
-                icon = <FontAwesomeIcon style={{ color: "#61DBFB" }} icon={faReact} />
+                icon = <a rel='noreferrer' target='_blank' href='https://reactjs.org/'><FontAwesomeIcon className='fa-2x' style={{ color: "#61DBFB" }} icon={faReact}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
-            else if (item === "JS") {
-                icon = <FontAwesomeIcon style={{ color: "#EAD64E" }} icon={faJsSquare} />
+            else if (item === "JavaScript") {
+                icon = <a rel='noreferrer' target='_blank' href='https://developer.mozilla.org/en-US/docs/Web/javascript'><FontAwesomeIcon className='fa-2x' style={{ color: "#EAD64E" }} icon={faJsSquare}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
             else if (item === "Bootstrap") {
-                icon = <FontAwesomeIcon style={{ color: "#543B79" }} icon={faBootstrap} />
+                icon = <a rel='noreferrer' target='_blank' href='https://getbootstrap.com/'><FontAwesomeIcon className='fa-2x' style={{ color: "#543B79" }} icon={faBootstrap}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
             else if (item === "HTML") {
-                icon = <FontAwesomeIcon style={{ color: "#E54C21" }} icon={faHtml5} />
+                icon = <a rel='noreferrer' target='_blank' href='https://developer.mozilla.org/en-US/docs/Web/HTML'><FontAwesomeIcon className='fa-2x' style={{ color: "#E54C21" }} icon={faHtml5}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
             else if (item === "CSS") {
-                icon = <FontAwesomeIcon style={{ color: "#264EE4" }} icon={faCss3Alt} />
+                icon = <a rel='noreferrer' target='_blank' href='https://developer.mozilla.org/en-US/docs/Web/CSS'><FontAwesomeIcon className='fa-2x' style={{ color: "#264EE4" }} icon={faCss3Alt}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
             else if (item === "GitHub") {
-                icon = <FontAwesomeIcon icon={faGithub} />
+                icon = <a rel='noreferrer' target='_blank' href='https://github.com/'><FontAwesomeIcon className='fa-2x' style={{ color: '#000000' }} icon={faGithub}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
             else if (item === "PHP") {
-                icon = <FontAwesomeIcon style={{ color: "#8690BA" }} icon={faPhp} />
+                icon = <a rel='noreferrer' target='_blank' href='https://www.php.net/'><FontAwesomeIcon className='fa-2x' style={{ color: "#8690BA" }} icon={faPhp}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
             else if (item === "Laravel") {
-                icon = <FontAwesomeIcon style={{ color: "#E62D1F" }} icon={faLaravel} />
+                icon = <a rel='noreferrer' target='_blank' href='https://laravel.com/'><FontAwesomeIcon className='fa-2x' style={{ color: "#E62D1F" }} icon={faLaravel}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
             else if (item === "Firebase") {
-                icon = <FontAwesomeIcon icon={faFireAlt} style={{ color: "#F99504" }} />
+                icon = <a rel='noreferrer' target='_blank' href='https://firebase.google.com/'><FontAwesomeIcon className='fa-2x' icon={faFireAlt} style={{ color: "#F99504" }}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
             else if (item === "MySQL") {
-                icon = <FontAwesomeIcon icon={faDatabase} style={{ color: "#686A71" }} />
+                icon = <a rel='noreferrer' target='_blank' href='https://www.mysql.com/'><FontAwesomeIcon className='fa-2x' icon={faDatabase} style={{ color: "#686A71" }}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
             else if (item === "Git") {
-                icon = <FontAwesomeIcon style={{ color: "#E74D33" }} icon={faGitAlt} />
+                icon = <a rel='noreferrer' target='_blank' href='https://git-scm.com/'><FontAwesomeIcon className='fa-2x' style={{ color: "#E74D33" }} icon={faGitAlt}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
             }
-            else {console.log('none')}
+            else if (item === "Heroku") {
+                icon = <a rel='noreferrer' target='_blank' href='https://www.heroku.com/'><FontAwesomeIcon className='fa-2x' style={{ color: "#6D3EA3" }} icon={faKaggle}
+                    data-bs-toggle="tooltip" data-bs-placement="bottom" title={item} /></a>
+            }
+            else if (item === "Currently Refactoring to ReactJS") {
+                icon = <a rel='noreferrer' target='_blank' href='https://www.heroku.com/'><FontAwesomeIcon className='fa-2x' style={{ color: "#FFC60A" }} icon={faExclamationTriangle}
+                data-bs-toggle="tooltip" data-bs-placement="bottom" title={item}/></a>
+            }
 
             return (
                 <div className="col-sm-2 fa-lg mt-2 pl-0 d-inline" >
@@ -61,17 +76,17 @@ function ProjectExt() {
         <Container>
             <Row>
                 <Col className="col-md-8 mt-4">
-                    {projectData.map((item, idx) => {
+                    {projectData.map((project, idx) => {
                         return (
-                            <Row className="mb-3 key={idx}">
+                            <Row className='mb-3' key={idx}>
                                 <Card className='text-left ml-4 border-0'>
                                     <CardBody>
-                                        <CardTitle tag="h1" className='display-4'><a href={item.demo} rel='noreferrer' target='_blank'>{item.title}</a></CardTitle>
-                                        <CardText tag='h4' className='my-2'>{item.description}</CardText>
-                                        <CardText className='my-2'>{splitStack(item.techs)}</CardText>
+                                        <CardTitle tag="h1" className='display-4'><a href={project.demo} rel='noreferrer' target='_blank'>{project.title}</a></CardTitle>
+                                        <CardText tag='h4' className='my-3'>{project.description}</CardText>
+                                        <CardText className='my-3'>{tech(project.techs)}</CardText>
                                         <Row>
-                                            <Button href={item.demo} target="_blank" className='mx-3'>Demo</Button>
-                                            <Button href={item.github} target="_blank" className='mx-2'>Github</Button>
+                                            <Button href={project.demo} target="_blank" className='mx-3'>Demo</Button>
+                                            <Button href={project.github} target="_blank" className='mx-2'>Github</Button>
                                         </Row>
                                     </CardBody>
 
--- src/projectData.js
+++ src/projectData.js
@@ -4,49 +4,49 @@ const projectData = [
         "description": "Capstone Project: Youth Sports League Management App utilizing the complete stack of tools learned throughout bootcamp.",
         "github": "https://github.com/dreesehudson/aotc_react",
         "demo": "",
-        "techs": ['React','JS','Bootstrap', 'CSS', 'HTML', 'PHP', 'Laravel', 'MySQL', 'Git', 'Firebase' ]
+        "techs": ['React','JavaScript','Bootstrap', 'CSS', 'HTML', 'PHP', 'Laravel', 'MySQL', 'Git', 'Firebase', 'Heroku' ]
     },
     {
         "title": "reactRestaurant",
         "description": "Mock Restaurant Menu using ReactJS and a Menu API",
         "github": "https://github.com/dreesehudson/react-restaurant",
         "demo": "https://gusteaus-restaurant.web.app/",
-        "techs": ['React','JS','Bootstrap', 'CSS', 'HTML', 'Git', 'Firebase' ]
+        "techs": ['React','Bootstrap', 'Git', 'Firebase' ]
     },
     {
         "title": "fifteenPuzzle",
         "description": "Classic Puzzle Game created using React.",
         "github": "https://github.com/dreesehudson/puzzle-slider",
         "demo": "https://puzzle-slider-a8bfe.web.app/",
-        "techs": ['React','JS','Bootstrap', 'CSS', 'HTML', 'Git', 'Firebase' ]
+        "techs": ['React','Bootstrap', 'Git', 'Firebase' ]
     },
     {
         "title": "toDoList",
         "description": "Task Tracking App using React framework and Bootstrap for styling.",
         "github": "https://github.com/dreesehudson/to-do-list-react",
         "demo": "https://todolist-d0960.web.app/",
-        "techs": ['React','JS','Bootstrap', 'CSS', 'HTML',  'Git', 'Firebase' ]
+        "techs": ['React','Bootstrap', 'Git', 'Firebase' ]
     },
     {
         "title": "classyCalculator",
-        "description": "Basic arithmetic calculator utilizing Javascript Classes, refactored using React",
-        "github": "",
-        "demo": "",
-        "techs": ['React','JS','Bootstrap', 'CSS', 'HTML',  'Git', 'Firebase' ]
+        "description": "Basic arithmetic calculator utilizing Javascript Classes",
+        "github": "https://github.com/dreesehudson/classic-calculator",
+        "demo": "https://dreesehudson.github.io/classic-calculator/",
+        "techs": ['JavaScript','Bootstrap', 'CSS', 'HTML',  'Git', 'GitHub', 'Currently Refactoring to ReactJS' ]
     },
     {
         "title": "ticTacToeGame",
-        "description": "Responsive Tic-Tac-Toe game, emphasizes event.target to manipulate state, refactored using React.",
-        "github": "",
-        "demo": "",
-        "techs": ['React','JS','Bootstrap', 'CSS', 'HTML', 'Git', 'Firebase' ]
+        "description": "Responsive Tic-Tac-Toe game, emphasizes event.target to manipulate state, Currently Refactoring to ReactJSed using React.",
+        "github": "https://github.com/dreesehudson/tic-tac-toe",
+        "demo": "https://dreesehudson.github.io/tic-tac-toe/",
+        "techs": ['JavaScript','Bootstrap', 'CSS', 'HTML', 'Git', 'GitHub', 'Currently Refactoring to ReactJS']
     },
     {
         "title": "weatherAPIApp",
-        "description": "Interactive zip-code based weather app, refactored using React. ",
-        "github": "",
-        "demo": "",
-        "techs": ['React','JS','Bootstrap', 'MySQL', 'Git', 'Firebase']
+        "description": "Interactive zip-code based weather app. ",
+        "github": "https://github.com/dreesehudson/weather-app",
+        "demo": "https://dreesehudson.github.io/weather-app/",
+        "techs": [/*'React',*/'JavaScript','Bootstrap', 'CSS', 'HTML', 'Git', 'GitHub', 'Currently Refactoring to ReactJS']
     },
     {
         "title": "campaignComponents",
@@ -57,17 +57,17 @@ const projectData = [
     },
     {
         "title": "mindReaderGame",
-        "description": "Interactive mind reading game, refactored using React",
-        "github": "",
-        "demo": "",
-        "techs": ['React','JS','Bootstrap', 'CSS', 'HTML', 'Git', 'Firebase']
+        "description": "Interactive mind reading game",
+        "github": "https://github.com/dreesehudson/mind-reader",
+        "demo": "https://dreesehudson.github.io/mind-reader/",
+        "techs": [/*'React',*/ 'JavaScript','Bootstrap', 'CSS', 'HTML', 'Git','GitHub', 'Currently Refactoring to ReactJS']
     },
     {
         "title": "bouncingBalls",
         "description": "Javascript Exercise from Github",
         "github": "https://github.com/dreesehudson/bouncing-balls",
         "demo": "https://dreesehudson.github.io/bouncing-balls/",
-        "techs": ['JS', 'CSS', 'HTML', 'Git', 'GitHub']
+        "techs": ['JavaScript', 'Git', 'GitHub']
     }
 ]
 export default projectData;
